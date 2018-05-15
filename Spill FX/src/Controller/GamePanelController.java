42a8ea9111928a3baa37a44a5444c1bb1bccf9f9
M. Furkan Ergin <furkann98@hotmail.com>
2018-05-15 21:36:43 +0200

    sånn

--- Spill FX/src/Controller/GamePanelController.java
+++ Spill FX/src/Controller/GamePanelController.java
@@ -1,7 +1,6 @@
 package Controller;
 
 
-
 import Logic.*;
 import Logic.GameSave;
 import javafx.animation.AnimationTimer;
@@ -50,7 +49,6 @@ import javafx.scene.control.Button;
  * @author Muhammed Furkan Ergin s325881 / Pedram Rahdeirjoo s325906
  */
 public class GamePanelController implements Initializable {
-
 
 
     //DATAFELT
